f4968cb9a5ff1ffc2d52210e131a07476500f1fd
Dmitriy Deryabin <dmitriy@ustisha.ru>
2021-04-25 16:52:46 +0700

    Separated library for radio network support

---
+++ include/SmartNet.h
@@ -0,0 +1,41 @@
+//#define SERIAL_DEBUG
+
+#ifndef SMARTNET_H
+#define SMARTNET_H
+
+class NetComponent;
+
+#include <Arduino.h>
+#include <DebugLog.h>
+#include <Net.h>
+#include <RadioInterface.h>
+
+class SmartNet {
+    struct NetComponents {
+        NetComponents() {
+            netComponent = nullptr;
+        }
+
+        NetComponent *netComponent;
+    };
+
+public:
+    uint8_t sender;
+
+    SmartNet(uint16_t s, uint8_t max) : sender(s),
+                                        components(new NetComponents[max]{}),
+                                        maxCmp(max) {}
+
+    void sendInfo(RadioInterface *n, long data, uint8_t cmd = CMD_INFO);
+
+    void addNetComponent(NetComponent *nc);
+
+    void commandReceived(Packet *p);
+
+protected:
+    NetComponents *components;
+    uint8_t maxCmp;
+    uint8_t i = 0;
+};
+
+#endif //SMARTNET_H
