6630abeaa91f4ed509e76844840616ced0a2db25
Dmitriy Deryabin <d.deryabin@tektorg.ru>
2023-09-16 16:18:17 +0700

    New component.

--- include/Net.h
+++ include/Net.h
@@ -84,7 +84,8 @@ const uint16_t BATHROOM_FLOOR = 011;
 // @deprecated
 const uint16_t BATHROOM_VALVE = 021;
 // 031 Not working!
-const uint16_t COLD_CHAMBER = 041;
+const uint16_t WARDROBE = 041;
+const uint16_t PORCH = 0141;
 // 02
 // @todo Move to 014
 const uint16_t OUTSIDE_TEMP = 02;
